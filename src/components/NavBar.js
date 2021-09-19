62a0a858c68408383238d5afe820d3e19ad0eb53
n8ew <susejano@gmail.com>
2021-09-19 13:21:39 +0100

    wee style chages to navbar, still waiting for logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,9 @@
 import React, { useContext } from 'react'
 import DbDataContext from '../context/dbData/dbDataContext'
 
+import { useTheme } from '@material-ui/core'
+import { useMediaQuery } from '@material-ui/core'
+
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 
@@ -8,7 +11,7 @@ import { makeStyles } from '@material-ui/core'
 
 const useStyles = makeStyles({
    text: {
-      color: '#023047'
+      color: '#eee'
    }
 })
 
@@ -17,13 +20,23 @@ const NavBar = () => {
    const dbDataContext = useContext(DbDataContext)
    const { isLogged, logoutAdmin } = dbDataContext
 
+   const theme = useTheme()
+   const isLg = useMediaQuery(theme.breakpoints.only('lg')) 
+   const isEl = useMediaQuery(theme.breakpoints.only('xl'))
+   const bigScreen = () => {
+      if( isLg || isEl ) {
+         return true
+      }
+      return false
+   }
+
    const classes = useStyles()
 
    const navboxStyle = {
       display: "flex",
       justifyContent: "space-between",
       alignItems: 'center',
-      width: "80%",
+      width:  bigScreen ? "60%":"80%",
       height: "10vh",
    }
    const navStyle = {
@@ -46,7 +59,7 @@ const NavBar = () => {
             </Typography>
             <Button 
                className={ classes.text }
-               style={{ visibility: isLogged? "visible": "hidden"}}
+               style={{ visibility: isLogged? "visible": "hidden" , fontWeight: 'bold'}}
                onClick={ logoutAdmin }
             >Wyloguj</Button>
          </div>
